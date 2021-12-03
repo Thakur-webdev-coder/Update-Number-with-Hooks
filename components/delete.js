6add137786f2217c6247c2b70a4036195b1ded34
shivenj <shiven.juneja@gmail.com>
2021-12-03 17:41:44 +0530

    changes

---
+++ components/delete.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Mydel = () => {
+  return (
+    <View>
+      <Text>File For Delete</Text>
+    </View>
+  )
+}
+
+export default Mydel;
+
+const styles = StyleSheet.create({})
